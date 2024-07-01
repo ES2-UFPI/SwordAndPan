2693edcda13af4942806ab654ebdbac46a065e65
Eryck Kawa <okazakiall114@gmail.com>
2024-07-01 17:49:21 -0300

    adicionado sistema de roll no player

--- Assets/Scripts/Input/GameInput.cs
+++ Assets/Scripts/Input/GameInput.cs
@@ -4,49 +4,40 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
-    // Eventos que serão disparados quando as ações de interagir e atirar forem realizadas
-    public event EventHandler OnInteractAction;
-    public event EventHandler OnFireAction;
+    public event EventHandler OnInteractAction; // Evento de interação
+    public event EventHandler OnFireAction; // Evento de ataque
+    public event EventHandler OnRollAction; // Evento de rolamento
 
-    // Referência para as ações de entrada do jogador, definida pela classe PlayerInputActions gerada automaticamente
     private PlayerInputActions playerInputActions;
 
-    // Método chamado quando o script é inicializado
     private void Awake()
     {
-        // Instancia as ações de entrada do jogador e as habilita
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable(); // Habilita as ações de entrada definidas na seção Player
 
-        // Adiciona handlers para os eventos "performed" das ações "Interact" e "Attack"
-        playerInputActions.Player.Interact.performed += Interact_performed;
-        playerInputActions.Player.Attack.performed += Fire_performed;
+        playerInputActions.Player.Interact.performed += Interact_performed; // Liga a ação de interação
+        playerInputActions.Player.Attack.performed += Fire_performed; // Liga a ação de ataque
+        playerInputActions.Player.Roll.performed += Roll_performed; // Liga a ação de rolamento
     }
 
-    // Método chamado quando a ação de interagir é realizada
     private void Interact_performed(InputAction.CallbackContext obj)
     {
-        // Dispara o evento OnInteractAction, se houver algum inscrito
-        OnInteractAction?.Invoke(this, EventArgs.Empty);
+        OnInteractAction?.Invoke(this, EventArgs.Empty); // Dispara o evento de interação
     }
 
-    // Método chamado quando a ação de atirar é realizada
     private void Fire_performed(InputAction.CallbackContext obj)
     {
-        // Dispara o evento OnFireAction, se houver algum inscrito
-        OnFireAction?.Invoke(this, EventArgs.Empty);
+        OnFireAction?.Invoke(this, EventArgs.Empty); // Dispara o evento de ataque
     }
 
-    // Método para obter o vetor de movimento normalizado
+    private void Roll_performed(InputAction.CallbackContext obj)
+    {
+        OnRollAction?.Invoke(this, EventArgs.Empty); // Dispara o evento de rolamento
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
-        // Obtém o vetor de movimento do jogador a partir das ações de entrada
         Vector2 inputSystem = playerInputActions.Player.Move.ReadValue<Vector2>();
-
-        // Normaliza o vetor de movimento
-        inputSystem = inputSystem.normalized;
-
-        // Retorna o vetor de movimento normalizado
-        return inputSystem;
+        return inputSystem.normalized;
     }
 }
