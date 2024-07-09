7db93a052b9f526e167bc608a46a545c1b9cfbb3
marcio-leal <108641644+marcio-leal@users.noreply.github.com>
2024-07-09 08:21:46 -0300

    Implementação inicial do Sistema de Inventário
    Implementação básica e apenas lógica do sistema de inventário.

---
+++ Assets/Scripts/Inventario.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventario : MonoBehaviour
+{
+    Dictionary<String, int> mapaInventario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO operação pra carregar inventario de um save
+        //operação para inicializar os inventários com tamanhos corretos de items
+        mapaInventario.Add("Maçã", 0);
+        mapaInventario.Add("Carne", 0);
+        mapaInventario.Add("Banana", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void interagirComDict(String nome, int quantia){
+        //quantia pode ser negativa
+        mapaInventario[nome] = mapaInventario[nome] + quantia;
+    }   
+}
