d64a5bf8013c7f15601c7374a18f432a9e5932bf
Eryck Kawa <108304916+EryckKawa@users.noreply.github.com>
2024-07-09 08:44:48 -0300

    update playerstats script

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -2,5 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[SerializeField] private float playerHealth; 
-[SerializeField] private float damagePlayer; 
+public class PlayerStatus : MonoBehaviour
+{
+    [SerializeField] private float playerHealth = 100f; // Valor inicial da saúde do jogador
+    [SerializeField] private float damagePlayer = 10f; // Dano causado pelo jogador
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(damagePlayer);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+        Debug.Log($"Player took {damage} damage. Current health: {playerHealth}");
+        
+        if (playerHealth <= 0)
+        {
+            Debug.Log("Player has died!");
+        }
+    }
+}
