71a1676e2ca02fcd236d4adfccef56295f5318fc
Wesley Coutinho <117856549+sousaawesley@users.noreply.github.com>
2024-07-18 09:40:57 -0300

    Logica do sistema de salvamento(Em andamento)
    Logica do sistema de salvamento(Em andamento)

---
+++ Assets/Scripts/Item/InventoryManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+    public List<Item> Items = new List<Item>();
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+}
