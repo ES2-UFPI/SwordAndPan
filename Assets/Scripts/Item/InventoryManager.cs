e2beb1a604450e4c4d29e7700b6f8acf84956f5b
Wesley Coutinho <117856549+sousaawesley@users.noreply.github.com>
2024-07-23 00:55:00 -0300

    Sistema de Inventário - Sistema de Salvamento #35
    Sistema de Inventário - Sistema de Salvamento #35

--- Assets/Scripts/Item/InventoryManager.cs
+++ Assets/Scripts/Item/InventoryManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
     public List<Item> Items = new List<Item>();
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+    public Toggle EnableRemove;
+    public InventoryItemController[] InventoryItems;
     private void Awake()
     {
         Instance = this;
@@ -19,5 +25,51 @@ public class InventoryManager : MonoBehaviour
     public void Remove(Item item)
     {
         Items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        //Clean
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            var ItemIcon = obj.transform.Find("ItemName").GetComponent<Image>();
+            var RemoveButton = obj.transform.Find("RemoveButton").gameObject;
+
+            itemName.text = item.itemName;
+            ItemIcon.sprite = item.icon;
+
+            if(EnableRemove.isOn)
+                RemoveButton.gameObject.SetActive(true);
+        }
+
+        SetInventoryItems();
+    }
+
+    public void EnableItemsRemove()
+    {
+        if (EnableRemove.isOn)
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetInventoryItems()
+    {
+        InventoryItems = ItemContent.GetComponentsInChildren<InventoryItemController>();
+
+        for (int i=0; i < Items.Count; i++)
+        {
+            InventoryItems[i].AddItem(Items[i]);
+        }
     }
 }
