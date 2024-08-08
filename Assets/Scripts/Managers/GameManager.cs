2df927b0db3362b48f0104921f923496d7098e6d
Eryck Kawa <okazakiall114@gmail.com>
2024-08-08 00:24:30 -0300

    add main menu scene

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,19 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // Method to load a scene by name
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Method to exit the game
+    public void ExitGame()
+    {
+        // This will only work in a built application
+        // It won't do anything in the Unity editor
+        Application.Quit();
     }
 }
